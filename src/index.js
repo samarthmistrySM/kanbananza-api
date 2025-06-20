0a130e252074922a3d23c34b916631e11c365459
Mistry Samarth Dilpesh <samarth.dilpesh@magicsw.com>
2025-06-20 13:32:02 +0530

    restart

--- src/index.js
+++ src/index.js
@@ -1,63 +1,11 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import swaggerUi from "swagger-ui-express";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const swaggerPath = path.join(__dirname, "../swagger-output.json");
-
-if (!fs.existsSync(swaggerPath)) {
-  const { default: swaggerAutogen } = await import("swagger-autogen");
-
-  const doc = {
-    info: {
-      title: "Kanbananza",
-      description: "Kanbananza a Scrum board API",
-    },
-    host:
-      process.env.NODE_ENV === "production"
-        ? "https://kanbananza-api.vercel.app/api"
-        : "localhost:3000/api",
-    schemes: [process.env.NODE_ENV === "production" ? "https" : "http"],
-    securityDefinitions: {
-      bearerAuth: {
-        type: "apiKey",
-        name: "Authorization",
-        in: "header",
-        description: "Enter your bearer token as **Bearer &lt;token&gt;**",
-      },
-    },
-  };
-
-  const routes = ["./src/routes/index.js"];
-  await swaggerAutogen()(swaggerPath, routes, doc);
-}
-
-const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath));
-const css = fs.readFileSync(
-  path.resolve(__dirname, "../node_modules/swagger-ui-dist/swagger-ui.css"),
-  "utf8"
-);
-
-const options = {
-  customCss: css,
-  customfavIcon: "/api-doc/swagger-ui/favicon-32x32.png",
-  customSiteTitle: "Kanbananza API",
-  swaggerOptions: {
-    url: "/api-doc/swagger.json",
-  },
-};
-
-import express from "express";
+import express from "express"
 import dotenv from "dotenv";
-
+import swaggerUi from "swagger-ui-express"
 import cors from "cors";
 import routes from "./routes/index.js";
-import connectDb from "./config/connectDB.js";
-import errorHandler from "./middleware/errorHandler.js";
-
-const isVercel = process.env.VERCEL === "1";
+import connectDb from "./config/connectDB.js"
+import errorHandler from "./middleware/errorHandler.js"
+import swaggerDocument from "../swagger-output.json" with {type: 'json'};
 
 dotenv.config();
 const app = express();
@@ -66,31 +14,22 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api-doc/swagger-ui", express.static("node_modules/swagger-ui-dist"));
-app.use(
-  "/api-doc",
-  swaggerUi.serveFiles(swaggerDocument, { redirect: false }),
-  swaggerUi.setup(swaggerDocument, options)
-);
-
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", routes());
 app.use(errorHandler);
 
-const startServer = async () => {
-  try {
-    await connectDb(process.env.MONGO_URL);
 
-    if (!isVercel) {
-      app.listen(PORT, () => {
-        console.log(`🚀 Server is running on http://localhost:${PORT}`);
-      });
+const startServer = async () => {
+    try {
+        await connectDb(process.env.MONGO_URL);
+
+        app.listen(PORT, async () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error.message);
+        process.exit(1);
     }
-  } catch (error) {
-    console.error("Error starting server:", error.message);
-    process.exit(1);
-  }
-};
+}
 
 startServer();
-
-export default app;
