de7285d8c87e5148a512888b443f0c1ec40485a9
Mistry Samarth Dilpesh <samarth.dilpesh@magicsw.com>
2025-06-20 11:43:33 +0530

    Samarth: deployment swagger bug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./swagger.js"
 import express from "express"
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express"
--- src/swagger.js
+++ src/swagger.js
@@ -1,18 +1,21 @@
 import swaggerAutogen from "swagger-autogen";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const doc = {
   info: {
     title: "Kanbananza",
     description: "Kanbananza a Scrum board API",
   },
-  host: "localhost:3000/api",
+  host: isProd ? "https://kanbananza-api.vercel.app/api" : "localhost:3000/api",
   schemes: ["http"],
   securityDefinitions: {
     bearerAuth: {
       type: "apiKey",
       name: "Authorization",
       in: "header",
-      description: "Enter your bearer token in the format **Bearer &lt;token&gt;**"
+      description:
+        "Enter your bearer token in the format **Bearer &lt;token&gt;**",
     },
   },
 };
